023ac10de1f8df5b8fd2fdbe220db39e49e3875a
Agustin Cidoni <agustincidoni@gmail.com>
2025-09-05 21:30:45 -0300

    CVS loader

--- src/Servicio.java
+++ src/Servicio.java
@@ -1,15 +1,44 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.practico.dao.factory.DAOfactory;
 import org.practico.dao.interfaces.ClienteDAO;
 
 public class Servicio {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		DAOfactory mysqlDAO = DAOfactory.getDAOfactory(1);
 		mysqlDAO.connect();
 		//mysqlDAO.loadDB();
 		ClienteDAO ClienteDAO = mysqlDAO.getCLienteDAO();
-		ClienteDAO.insertCliente(1, "Agus", "agus@mail.com");
 		System.out.println(ClienteDAO.getCliente(1));
+		loadClients(ClienteDAO);
+		System.out.println(ClienteDAO.getCliente(1));
+	}
+	
+	public static void loadClients(ClienteDAO c) {
+
+		CSVParser parser;
+		try {
+			parser = CSVFormat.DEFAULT.withHeader().parse(new
+					FileReader("src/clientes.csv"));
+			for(CSVRecord row: parser) {
+				c.insertCliente(
+			            Integer.parseInt(row.get("idCliente")),
+			            row.get("nombre"),
+			            row.get("email")
+			        );
+				}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		
 	}
 
 }
